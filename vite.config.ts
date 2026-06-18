09d857a6965b9cc55186b8d4975bd2d6fb6aba57
Kelsey Huse <kelseyhuse30@gmail.com>
2026-06-18 15:28:00 -0500

    test deploy dev

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/felt-sdk-testing' : '/felt-sdk-testing/dev/',
   plugins: [react()],
-});
+}));
