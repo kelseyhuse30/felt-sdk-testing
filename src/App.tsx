275980b7a3d5c4e3c06ac62622070403e5dbbc74
Kelsey Huse <kelseyhuse30@gmail.com>
2026-06-18 15:38:54 -0500

    add MPH reference

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import { ViewportInfo } from "./components/ViewportInfo";
 import { FeltContext, useFeltEmbed } from "./feltUtils";
 
 export default function Page() {
-  const { felt, mapRef } = useFeltEmbed("w9BxV0EmdR2u5C4AyrTke9CB", {
+  const { felt, mapRef } = useFeltEmbed("2pjfh9ClOTymZRWajPdjFoA", {
     uiControls: {
       cooperativeGestures: false,
       fullScreenButton: false,
